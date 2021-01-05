fee33233ec49d7ed6799b22afd5874e3846813eb
VS <mixworldapp@gmail.com>
2021-01-05 23:51:47 +0600

    fix url adress

--- test_items.py
+++ test_items.py
@@ -1,4 +1,4 @@
-link = "http://selenium1py.pythonanywhere.com/ru/catalogue/coders-at-work_207/"
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 def test_should_see_basket_button(browser):
     browser.get(link)
